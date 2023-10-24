25da34274238194e599aebaa4df265aa08a76b0e
Joud Awad <joud.awad@metalabdesign.com>
2023-10-24 23:18:28 +0300

    remove public db and keep the private one

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const express = require("express");
 const axios = require("axios");
 const mysql = require("mysql");
-const { Pool } = require("pg");
 
 const app = express();
 const PORT = 3000;
-
-const dbConfigPublic = {
-  host: "database-1.cpgwvhcnpddn.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "password",
-  port: 3306,
-  database: "db_default",
-};
 
 const dbConfigPrivate = {
   host: "dev-catalog.cpgwvhcnpddn.us-east-2.rds.amazonaws.com",
@@ -23,15 +14,6 @@ const dbConfigPrivate = {
 };
 
 let connection;
-let connection2;
-
-const pool = new Pool({
-  user: "postgres",
-  host: "db-test.cpgwvhcnpddn.us-east-2.rds.amazonaws.com",
-  database: "dev_db",
-  password: "password",
-  port: 5432,
-});
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Home page of package 3!");
@@ -45,21 +27,6 @@ app.get("/db/req", (req, res) => {
 
 app.get("/db/env-variables", (req, res) => {
   res.send(process.env);
-});
-
-app.get("/db/postgres", async (req, res) => {
-  pool.connect((err, client, release) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send(err);
-    }
-
-    console.log("Connected to PostgreSQL database!");
-
-    // Release the client when you're finished with it
-    release();
-    return res.status(200).json({ message: "connected successfully" });
-  });
 });
 
 app.get("/db/verify-connection", (req, res) => {
@@ -89,23 +56,9 @@ app.get("/db/call-endpoint", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!!`);
   try {
-    connection = mysql.createConnection(dbConfigPublic);
+    connection = mysql.createConnection(dbConfigPrivate);
     // Check if the connection is successful
     connection.connect((err) => {
-      if (err) {
-        console.error("Error connecting to the public MySQL database:", err);
-      } else {
-        console.log("Successfully connected to the MySQL database.");
-      }
-    });
-  } catch (error) {
-    console.error("Error starting up the app", error);
-  }
-
-  try {
-    connection2 = mysql.createConnection(dbConfigPrivate);
-    // Check if the connection is successful
-    connection2.connect((err) => {
       if (err) {
         console.error("Error connecting to the private MySQL database:", err);
       } else {
